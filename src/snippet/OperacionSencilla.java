1a497030a60caabc76661960b95b263d992ff3e1
ruizd <ruizd@DESKTOP-FP96FRU.localdomain>
2025-05-04 22:11:17 +0200

    Primer commit

---
+++ src/snippet/OperacionSencilla.java
@@ -0,0 +1,13 @@
+public class OperacionSencilla {
+
+    public static void main(String[] args) {
+
+        int numero1 = 10;
+        int numero2 = 5;
+
+
+        int resultado = numero1 + numero2;
+
+        System.out.println("La suma de " + numero1 + " y " + numero2 + " es: " + resultado);
+    }
+}
