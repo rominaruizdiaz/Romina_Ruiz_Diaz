2bbd15ff698305ef70d8ed71ec4d0b61928d1717
ruizd <ruizd@DESKTOP-FP96FRU.localdomain>
2025-05-04 22:24:54 +0200

    Nuevos cambios

--- src/snippet/OperacionSencilla.java
+++ src/snippet/OperacionSencilla.java
@@ -3,11 +3,11 @@ public class OperacionSencilla {
     public static void main(String[] args) {
 
         int numero1 = 10;
-        int numero2 = 5;
+        int numero2 = 33333;
 
 
         int resultado = numero1 + numero2;
 
-        System.out.println("La suma de " + numero1 + " y " + numero2 + " es: " + resultado);
+        System.out.println("La suma de " + numero1 + " y " + numero2 + " es!!! " + resultado);
     }
 }
